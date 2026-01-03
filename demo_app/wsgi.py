faf9e813663f75cfcb5adefe59b4506eb79e2588
Mehmet Şener <tekstilmoli@gmail.com>
2026-01-03 15:15:18 +0300

    Remove admin creation from wsgi

--- demo_app/wsgi.py
+++ demo_app/wsgi.py
@@ -1,9 +1,3 @@
-"""
-WSGI config for demo_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
