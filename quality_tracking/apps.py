6a41c69b365b68b2270f9b58a550da8e8005fbf3
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-04 23:30:22 +0300

    Kalite takip app config ekle

---
+++ quality_tracking/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class QualityTrackingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "quality_tracking"
+    verbose_name = "Kalite ve Şikayet Takibi"
