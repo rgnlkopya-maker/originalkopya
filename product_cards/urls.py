a789ff8481ee541400c2232f32416dd3da709e24
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-04 17:34:09 +0300

    Urun karti durum endpointini bagla

--- product_cards/urls.py
+++ product_cards/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, status_views
 
 urlpatterns = [
     path("", views.product_card_list, name="product_card_list"),
     path("kur-guncelle/", views.refresh_exchange_rate, name="refresh_exchange_rate"),
-    path("durum-degistir/", views.toggle_product_card_status, name="toggle_product_card_status"),
+    path("durum-degistir/", status_views.toggle_product_card_status, name="toggle_product_card_status"),
     path("<int:card_id>/", views.product_card_detail, name="product_card_detail"),
     path("malzemeler/", views.material_list, name="material_list"),
 ]
