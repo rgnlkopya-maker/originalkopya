44e6ef5b5fc347b1a02e1accf1d0f129ca4b0d8e
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-04 16:24:56 +0300

    Kur guncelleme endpointi ekle

--- product_cards/urls.py
+++ product_cards/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.product_card_list, name="product_card_list"),
+    path("kur-guncelle/", views.refresh_exchange_rate, name="refresh_exchange_rate"),
     path("<int:card_id>/", views.product_card_detail, name="product_card_detail"),
     path("malzemeler/", views.material_list, name="material_list"),
 ]
