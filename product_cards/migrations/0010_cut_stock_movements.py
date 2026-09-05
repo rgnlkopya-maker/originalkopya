350da83e72380373702f714090e9d829674ff024
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-05 11:57:41 +0300

    Fix cut stock migration dependency

--- product_cards/migrations/0010_cut_stock_movements.py
+++ product_cards/migrations/0010_cut_stock_movements.py
@@ -3,7 +3,7 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-    dependencies = [("core", "0052_finance_events_non_status"), ("product_cards", "0009_material_stock_management")]
+    dependencies = [("core", "0052_finans_hareketlerini_son_durumdan_ayir"), ("product_cards", "0009_material_stock_management")]
     operations = [
         migrations.AddField(model_name="materialstockmovement", name="order", field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name="material_stock_movements", to="core.order")),
         migrations.AddField(model_name="materialstockmovement", name="source_event", field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name="material_stock_movements", to="core.orderevent")),
