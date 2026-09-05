f5414b7ba98dc8dd90abe6635ad82723ad088126
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-05 21:45:03 +0300

    Add planning URLs

---
+++ planning/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.planning_page, name="planning_page"),
+    path("kaydet/", views.save_entry, name="planning_save_entry"),
+]
