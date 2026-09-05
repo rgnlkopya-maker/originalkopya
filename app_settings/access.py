3efb47fe3d9c9177c03e6c87be124ebc71223a20
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-05 08:54:20 +0300

    Merkezi kullanıcı yetki kontrolünü ekle

---
+++ app_settings/access.py
@@ -0,0 +1,30 @@
+from functools import wraps
+from django.http import HttpResponseForbidden
+from .models import UserAccess
+
+
+def get_access(user):
+    if not getattr(user, 'is_authenticated', False):
+        return None
+    access, _ = UserAccess.objects.get_or_create(user=user)
+    return access
+
+
+def has_access(user, permission_name):
+    if not getattr(user, 'is_authenticated', False):
+        return False
+    if user.is_superuser:
+        return True
+    access = get_access(user)
+    return bool(getattr(access, permission_name, False))
+
+
+def access_required(permission_name):
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapped(request, *args, **kwargs):
+            if not has_access(request.user, permission_name):
+                return HttpResponseForbidden('Bu bölüme erişim yetkiniz yok.')
+            return view_func(request, *args, **kwargs)
+        return wrapped
+    return decorator
