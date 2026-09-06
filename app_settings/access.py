c6f87fcd1e71ef0949908eea48c7c454c1b5f9e1
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-06 20:31:32 +0300

    Grant full access to patron and manager roles

--- app_settings/access.py
+++ app_settings/access.py
@@ -3,6 +3,9 @@ from django.http import HttpResponseForbidden
 from .models import UserAccess
 
 
+FULL_ACCESS_GROUPS = {"patron", "mudur"}
+
+
 def get_access(user):
     if not getattr(user, 'is_authenticated', False):
         return None
@@ -10,10 +13,18 @@ def get_access(user):
     return access
 
 
-def has_access(user, permission_name):
+def has_full_access(user):
     if not getattr(user, 'is_authenticated', False):
         return False
     if user.is_superuser:
+        return True
+    return user.groups.filter(name__in=FULL_ACCESS_GROUPS).exists()
+
+
+def has_access(user, permission_name):
+    if not getattr(user, 'is_authenticated', False):
+        return False
+    if has_full_access(user):
         return True
     access = get_access(user)
     return bool(getattr(access, permission_name, False))
