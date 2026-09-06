0132cb3e0e0e0a245fb1052d5ddf145f7184bc07
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-07 02:24:38 +0300

    Merge attendance migration branches

---
+++ attendance/migrations/0009_merge_0008_health_and_second_location.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("attendance", "0008_employeehrprofile_health_fields"),
+        ("attendance", "0008_workplacesettings_second_location"),
+    ]
+
+    operations = []
