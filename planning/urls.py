b5a95ad367017f2907705ed348260b660ab2247d
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-06 17:08:16 +0300

    Add shipment planning route

--- planning/urls.py
+++ planning/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.planning_page, name="planning_page"),
+    path("sevkiyat/", views.shipment_planning_page, name="shipment_planning_page"),
     path("kaydet/", views.save_entry, name="planning_save_entry"),
 ]
