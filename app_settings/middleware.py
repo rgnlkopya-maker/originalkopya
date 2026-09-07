e2c2cee7902d9d150189be22c44ae463b555e131
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-07 10:41:28 +0300

    Allow all authenticated users to use attendance QR punch

--- app_settings/middleware.py
+++ app_settings/middleware.py
@@ -25,6 +25,8 @@ class MoliAccessMiddleware:
             permission = 'can_view_reports'
         elif path.startswith('/product-costs/') or path.startswith('/urun-kartlari/'):
             permission = 'can_view_costs'
+        elif path in {'/attendance/scan/', '/attendance/punch/'}:
+            permission = None
         elif path.startswith('/attendance/'):
             permission = 'can_view_attendance'
         elif path.startswith('/planlama/'):
