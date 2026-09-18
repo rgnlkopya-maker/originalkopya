30b57dbe86943e8b1d7fe4edac4503498ecbe937
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-18 23:01:48 +0300

    Align module access checks with user permission matrix

--- product_cards/finance_views.py
+++ product_cards/finance_views.py
@@ -1,3 +1,4 @@
+from app_settings.access import has_access
 import json
 from decimal import Decimal, InvalidOperation
 
@@ -36,7 +37,7 @@ AMOUNT_REQUIRED = set(FINANCIAL_MOVEMENT_LABELS.keys())
 
 
 def _can_manage(user):
-    return user.is_superuser or user.groups.filter(name__in=["patron", "mudur"]).exists()
+    return has_access(user, "can_view_shipping_finance")
 
 
 def _parse_decimal(value):
