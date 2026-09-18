c03eb10b46f906981a0a60b73e1e954a584091b9
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-18 22:59:55 +0300

    Add per-feature permission storage

--- app_settings/models.py
+++ app_settings/models.py
@@ -66,6 +66,7 @@ class UserAccess(models.Model):
     can_manage_planning = models.BooleanField(default=False)
     can_manage_users = models.BooleanField(default=False)
     can_view_settings = models.BooleanField(default=False)
+    feature_permissions = models.JSONField(default=dict, blank=True)
 
     updated_at = models.DateTimeField(auto_now=True)
 
