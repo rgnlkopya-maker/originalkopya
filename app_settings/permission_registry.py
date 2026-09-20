2f494165185ba063b11dc210ea5ea45ba04a41fc
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-20 17:36:29 +0300

    Protect consignment stock routes

--- app_settings/permission_registry.py
+++ app_settings/permission_registry.py
@@ -167,6 +167,8 @@ _RULES = [
     (re.compile(r"^/hatirlatmalar/"), "operations.reminders"),
 
     (re.compile(r"^/depolar/hazirdan-ver/"), "stock.ready_transfer"),
+    (re.compile(r"^/konsinye/(?:ekle/)?$"), "stock.inventory"),
+    (re.compile(r"^/order/\d+/konsinye-karsila/$"), "stock.ready_transfer"),
     (re.compile(r"^/order/\d+/stok-ekle/$"), "stock.add_from_order"),
     (re.compile(r"^/depolar/"), "stock.depots"),
     (re.compile(r"^/stok/[^/]+/(?:malzeme|urun)/"), "stock.inventory"),
