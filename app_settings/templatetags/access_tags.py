c78a79e169ea8399ba78328556e35eed4c153ee1
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-20 18:08:56 +0300

    Add feature permission template filter

--- app_settings/templatetags/access_tags.py
+++ app_settings/templatetags/access_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from app_settings.access import has_access
+from app_settings.access import has_access, has_feature_access
 
 register = template.Library()
 
@@ -8,3 +8,8 @@ register = template.Library()
 @register.filter
 def can(user, permission_name):
     return has_access(user, permission_name)
+
+
+@register.filter
+def can_feature(user, feature_key):
+    return has_feature_access(user, feature_key)
