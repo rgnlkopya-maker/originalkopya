4eee8cb97d484a3e31b5355152bbebe41cfd3f94
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-21 22:51:59 +0300

    Ust menu erisim salteri template yardimcilarini ekle

--- app_settings/templatetags/access_tags.py
+++ app_settings/templatetags/access_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
-from app_settings.access import has_access, has_feature_access
+from app_settings.access import has_access, has_feature_access, has_full_access
+from app_settings.models import SystemSettings
 
 register = template.Library()
 
@@ -13,3 +14,13 @@ def can(user, permission_name):
 @register.filter
 def can_feature(user, feature_key):
     return has_feature_access(user, feature_key)
+
+
+@register.filter
+def is_management(user):
+    return has_full_access(user)
+
+
+@register.simple_tag
+def staff_access_enabled():
+    return SystemSettings.get_solo().staff_access_enabled
