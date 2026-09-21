44a39aeb887f02d3c4f75723623fe0fd94b03b7b
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-21 22:51:54 +0300

    Personel erisim salteri URL ekle

--- app_settings/urls.py
+++ app_settings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.settings_home, name='settings_home'),
+    path('personel-erisim-salteri/', views.toggle_staff_access, name='toggle_staff_access'),
 ]
