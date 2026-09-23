6375853ec9afb57a5fd1ef40f68045afba603840
rgnlkopya-maker <rgnlkopya@gmail.com>
2026-09-23 21:04:15 +0300

    Favoriler URLlerini ekle

--- app_settings/urls.py
+++ app_settings/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.settings_home, name='settings_home'),
     path('personel-erisim-salteri/', views.toggle_staff_access, name='toggle_staff_access'),
+    path('favoriler/', views.favorites_list, name='favorites_list'),
+    path('favoriler/degistir/', views.favorite_toggle, name='favorite_toggle'),
 ]
